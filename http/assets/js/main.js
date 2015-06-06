1fc8762819009132c013de2672d0e35445d03667
Bohdan Anderson <bohdan.anderson@gmail.com>
2015-06-06 19:36:13 -0400

    comming a broken thing :( have to get off train

--- http/assets/js/main.js
+++ http/assets/js/main.js
@@ -1,5 +1,5 @@
 var app = {
-	ip: "192.168.0.102:8000",
+	ip: "192.168.43.171:8000",
 
 	init: function() {
 
@@ -33,4 +33,40 @@ app.explain = {
 			window.location.replace("/pages/2-settype.html");
 		}
 	}
+}
+
+app.pairing = {
+	previous: null,
+	data: {
+		absolute: null,
+		gamma: null,
+		alpha: null,
+		beta: null,
+		type: null,
+		time: null
+	},
+	init: function() {
+		app.pairing.previous = new Date();
+		app.socket = io.connect(app.ip);
+		app.pairing.data.type = Cookies.get().type;
+		window.addEventListener("deviceorientation", app.pairing.move, true);
+		app.socket.on("setId", app.pairing.setId);
+	},
+	setId: function(data) {
+		app.pairing.id = data;
+		alert(app.pairing.id);
+	},
+	move: function(event) {
+		var now = new Date();
+		if (now - app.pairing.previous < 100) {
+			return false;
+		}
+		app.pairing.previous = now;
+		app.pairing.data.time = now;
+		app.pairing.data.absolute = event.absolute;
+		app.pairing.data.gamma = parseInt(event.gamma * 100) / 100;
+		app.pairing.data.alpha = parseInt(event.alpha * 100) / 100;
+		app.pairing.data.beta = parseInt(event.beta * 100) / 100;
+		app.socket.emit("looking_for_pairing", app.pairing.data);
+	},
 }
--- socket.js
+++ socket.js
@@ -9,24 +9,66 @@ var websocket = {
 	views: {
 		init: function() {
 			websocket.io.sockets.on('connection', function(socket) {
-
+				console.log(socket.id);
 				socket.on('data', function(data) {
 					data.id = socket.id;
 					console.log(data);
-
 					websocket.io.emit("reciver", data);
-				})
+				});
+
+				socket.on('looking_for_pairing', function(data) {
+					websocket.phones[socket.id] = data;
+					socket.emit("setId", socket.id);
+					if (websocket.pairing.count == 0) {
+						websocket.pairing.start();
+					}
+				});
 
 				socket.on("disconnect", function(data) {
 					console.log("disconnect " + socket.id);
-					websocket.io.emit("reciver", {
-						type: "disconnect",
-						id: socket.id
-					})
+					if (websocket.phones[socket.id]) {
+						delete websocket.phones[socket.id];
+					}
+					// websocket.io.emit("reciver", {
+					// 	type: "disconnect",
+					// 	id: socket.id
+					// })
 				})
 			});
 		}
 	},
+	pairing: {
+		start: function() {
+			websocket.pairing.interval = setInterval(websocket.pairing.beat, 90);
+		},
+		beat: function() {
+			websocket.pairing.count = websocket.pairing.countPhones();
+			if (websocket.pairing.count == 0) {
+				websocket.pairing.stop();
+			};
+
+			// we check each phone looking for matches
+			var out = ""
+			for (phone in websocket.phones) {
+				out += " " + phone;
+			}
+		},
+		stop: function() {
+			clearInterval(websocket.pairing.interval);
+		},
+		countPhones: function() {
+			var out = 0
+			for (phone in websocket.phones) {
+				++out;
+			}
+			return out;
+		},
+		foundPair: function(phone1, phone2) {
+			websocket.io.emit("paired", [phone1, phone2]);
+		},
+		count: 0
+	},
+	phones: {}
 }
 
 exports.socket = websocket;
